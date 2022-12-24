fe25194afc9a30f0d3ae6ce966a2df1a013d8e8c
yunho <kimyouknow@naver.com>
2022-12-24 12:09:38 +0900

    Feat: push method

--- datastructure/Heap/heap.test.ts
+++ datastructure/Heap/heap.test.ts
@@ -1,11 +1,32 @@
 import { MinHeap } from '@datastructure/Heap';
 
 describe('Heap datastructure test', () => {
-  describe('Initialize Heap', () => {
+  describe('Initialize Min Heap', () => {
     it('Create Heap instance', () => {
       const minHeap = new MinHeap();
       expect(minHeap).toBeInstanceOf(MinHeap);
       expect(minHeap.showEntireMinHeap()).toStrictEqual([null]);
+    });
+  });
+  describe('Push value to Min Heap', () => {
+    let minHeap: MinHeap;
+    let spyOnPush: jest.SpyInstance;
+    const mockValue = 4;
+    beforeEach(() => {
+      minHeap = new MinHeap();
+      spyOnPush = jest.spyOn(minHeap, 'push');
+      minHeap.push(mockValue);
+    });
+    afterEach(() => {
+      spyOnPush.mockClear();
+    });
+    it('Push method should be in call with 1 param', () => {
+      expect(spyOnPush).toBeCalledWith(mockValue);
+      expect(spyOnPush.mock.calls[0].length).toBe(1);
+    });
+    it("Instance arr's last value should be equal with param", () => {
+      const minHeapArr = minHeap.showEntireMinHeap();
+      expect(minHeapArr[minHeapArr.length - 1]).toBe(mockValue);
     });
   });
 });
--- datastructure/Heap/index.ts
+++ datastructure/Heap/index.ts
@@ -6,4 +6,12 @@ export class MinHeap {
   showEntireMinHeap() {
     return [...this.arr];
   }
+  push(value: number) {
+    this.arr.push(value);
+    return this;
+  }
 }
+
+const minHeap = new MinHeap();
+minHeap.push(1);
+console.log(minHeap.showEntireMinHeap());
